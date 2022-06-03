56d363c40032ac025656e4b0940f1fe655f0e3f6
ppunikim <yd62322@naver.com>
2022-06-03 17:32:24 +0900

    a

---
+++ 오라클/bookuser(2022-06-03).sql
@@ -0,0 +1,23 @@
+-- 여기는 bookuser 화면이다.
+
+CREATE TABLE tbl_books (
+
+    isbn        VARCHAR2(13)        PRIMARY KEY,
+    title       nVARCHAR2(50)       NOT NULL,
+    author      nVARCHAR2(50)       NOT NULL,
+    publisher   nVARCHAR2(50)       NOT NULL,
+    price       number              ,
+    discount    number              ,
+    description nVARCHAR2(2000)     ,
+    pubdate     VARCHAR2(10)        ,
+    link        VARCHAR2(125)       ,
+    image       VARCHAR2(125)       
+);
+
+SELECT *
+FROM tbl_books;
+
+INSERT INTO tbl_books(isbn, title, author, publisher, price )
+VALUES('934798579','고민의 답','글배우','햇살책방',13000);
+
+commit;
